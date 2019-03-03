3eff6ee8ac3576bf28dbd741aa76038e24cf8587
DRL36 <jnly36@163.com>
2019-03-02 17:25:39 -0800

    'cuda'

--- models.py
+++ models.py
@@ -64,12 +64,13 @@ class BiDAF(nn.Module):
         # s = c_emb.shape
         # cf_emb = torch.zeros(s[0],s[1],1,device='cuda')
         cwf = torch.unsqueeze(cwf, dim = 2)
+
+        cwf = cwf.type(torch.FloatTensor)
+        cwf.to('cuda')
         print(c_emb.shape)
         print(cwf.shape)
         print(c_emb.type())
         print(cwf.type())
-        cwf = cwf.type(torch.FloatTensor)
-        cwf.to('cuda')
         ct_emb = torch.cat((c_emb, cwf), dim = 2)
         # s = q_emb.shape
         # qf_emb = torch.zeros(s[0],s[1],1,device='cuda')
